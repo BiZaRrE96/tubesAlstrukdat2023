aac4242ee4c0d0bfdef52c72aa76582a063a74ee
BiZaRrE96 <91012945+BiZaRrE96@users.noreply.github.com>
2023-11-03 19:36:07 +0700

    Progress on kicau related functions

--- src/kicau/kicau.h
+++ src/kicau/kicau.h
@@ -2,11 +2,13 @@
 #define _KICAU_H_
 
 //ADT kicau(?)
+//MENGGUNAKAN : ADT LST DINAMIK
 #include <time.h>
 
 #include "../boolean/boolean.h"
 #include "../userlist/userlist.h"
 #include "../sentencemachine/sentencemachine.h"
+#include "../commandmachine/commandmachine.h"
 //#include "../time/time.h"
 
 //Suatu kicauan minimal terdiri dari 
@@ -27,6 +29,13 @@ typedef struct {
     int Like;
     id Author;
     struct tm Time;
+
+    //ini itungan nya node i guess? prep for balasan
+    //KICAU *parent;
+    //KICAU *children;
+
+    //membantu memberi spasi sebelum tulisan
+    //int indexLevel;
 } KICAU;
 
 typedef struct {
@@ -36,6 +45,10 @@ typedef struct {
 } KicauList;
 
 /* ***** SELEKTOR KICAU ***** */
+#define GetText(KL,i) (KL).Kicau[i].Text
+#define GetLike(KL,i) (KL).Kicau[i].Like
+#define GetAuthor(KL,i) (KL).Kicau[i].Author
+#define GetTime(KL,i) (KL).Kicau[i].Time
 
 /* ***** SELEKTOR KicauList ***** */
 #define CAPACITY(KL) (KL).capacity
@@ -43,10 +56,19 @@ typedef struct {
 #define GetKicauan(KL,i) (KL).Kicau[i]
 #define Buffer(KL) (KL).Kicau
 
-#define GetText(KL,i) (KL).Kicau[i].Text
-#define GetLike(KL,i) (KL).Kicau[i].Like
-#define GetAuthor(KL,i) (KL).Kicau[i].Author
-#define GetTime(KL,i) (KL).Kicau[i].Time
+//#define GetParent(K) (K).parent
+
+/* ***** MENGUBAH UKURAN ARRAY ***** */
+void expandList(KicauList *kl, int num);
+/* Proses : Menambahkan capacity l sebanyak num */
+/* I.S. List sudah terdefinisi */
+/* F.S. Ukuran list bertambah sebanyak num */
+
+void shrinkList(KicauList *kl, int num);
+/* Proses : Mengurangi capacity sebanyak num */
+/* I.S. List sudah terdefinisi, ukuran capacity > num, dan nEff < capacity - num. */
+/* F.S. Ukuran list berkurang sebanyak num. */
+
 
 /* ****** TEMPORARY FUNCTIONS, SUBJECT TO CHANGE WITH UPDATES*/
 void setToCurrentTime(struct tm * t){
@@ -56,6 +78,16 @@ void setToCurrentTime(struct tm * t){
     t = localtime(&currentTime);
 
 };
+
+static boolean AcanSeeB(id A, id B){
+    //returns true if :
+    // B is a public account
+    // B is a private account and A is friends with B
+    //check here
+    return true;
+}
+
+/* ***** MAIN FUNCTIONS ***** */
 
 void createKicauList (KicauList* KL, int cap){
     
@@ -79,26 +111,46 @@ void createKicauList (KicauList* KL, int cap){
 
 void Kicau(KicauList *KL, id author){
     printf("Masukkan kicauan: ");
-
     int i = Count(*KL);
-    StartSentence();
-    GetText(*KL,i) = SentenceToWord();
+    STARTCOMMAND();
+    GetText(*KL,i) = currentWord;
     GetLike(*KL,i) = 0;
     GetAuthor(*KL,i) = author;
     setToCurrentTime(&GetTime(*KL,i));
+
+
+
     Count(*KL)++;
 }
 
-/* ***** MENGUBAH UKURAN ARRAY ***** */
+void likeKicau(KicauList *KL,id postID, id author){
+    
+    GetLike(*KL,postID)++;
+}
 
-void expandList(KicauList *kl, int num);
-/* Proses : Menambahkan capacity l sebanyak num */
-/* I.S. List sudah terdefinisi */
-/* F.S. Ukuran list bertambah sebanyak num */
+//akan digunakan untuk nanti balasan
+void printSpaces(int x){
+    for (int i = 0; i < x; i++){
+        printf(" ");
+    }
+};
 
-void shrinkList(KicauList *kl, int num);
-/* Proses : Mengurangi capacity sebanyak num */
-/* I.S. List sudah terdefinisi, ukuran capacity > num, dan nEff < capacity - num. */
-/* F.S. Ukuran list berkurang sebanyak num. */
+//view kicauan with id X (real id from 1) as user with id user
+void printKicauXasA(KicauList KL, id x, id user){
+    x--;
+    if (AcanSeeB(user,GetAuthor(KL,x)) == true){
+        printf("|ID = %d\n",x+1);
+        printf("|%d PLACEHOLDER UNTIL USERSYS\n",GetAuthor(KL,x));
+        printf("|DATE\n");
+        printf("|%s\n",wordToStr(GetText(KL,x)));
+    }
+    //printBalasan
+};
+
+void viewRecentAsA(KicauList KL,id A){
+    for (int i = Count(KL); i > 0; i--){
+        printKicauXasA(KL, i, A);
+    }
+}
 
 #endif
