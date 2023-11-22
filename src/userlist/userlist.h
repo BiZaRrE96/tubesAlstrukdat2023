1392c6dfd41b82edfcad2823f80d9a8e6ccf56be
Christian Justin <christian.justin23@gmail.com>
2023-11-22 21:57:18 +0700

    Changes and fixes to user and userlist

--- src/userlist/userlist.h
+++ src/userlist/userlist.h
@@ -41,6 +41,7 @@ int listLengthUserlist(UserList U){
 /* Mengirimkan banyaknya elemen efektif list */
 /* Mengirimkan nol jika list l kosong */
 
+
 /* ********** OUTPUT ********** */
 /* Hanya untuk keperluan debugging */
 void displayUserList(UserList U) {
@@ -78,5 +79,136 @@ void insertLastUser(UserList *U, User user){
 /* Proses: Menambahkan val sebagai elemen terakhir List */
 /* I.S. List l boleh kosong, tetapi tidak penuh */
 /* F.S. val adalah elemen terakhir l yang baru */
+
+
+/* ********** ADT USERLIST ********** */
+void DAFTAR(UserList *users)
+{
+    if (!isLogin)
+    {
+        boolean NameCheck = false;
+        Word name;
+
+        while (!NameCheck)
+        {
+            printf("\n\nMasukkan nama:\n");
+            STARTCOMMAND();
+
+            if (currentWord.Length <= 20)
+            {
+                name = currentWord;
+                NameCheck = true;
+            }
+            else
+            {
+                NameCheck = false;
+                printf("Username tidak valid karena kepanjangan. Masukkan lagi yuk!");
+            }
+        }
+
+        int index = indexOfUser(*users, name);
+        Word pass;
+        boolean PassCheck = false;
+
+        if (index != -1)
+        {
+            printf("Wah, sayang sekali nama tersebut telah diambil.");
+            DAFTAR(users);
+        }
+
+        else
+        {
+            while (!PassCheck)
+            {
+                printf("\n\nMasukkan kata sandi:\n");
+                STARTCOMMAND();
+
+                if (currentWord.Length <= 20)
+                {
+                    pass = currentWord;
+                    PassCheck = true;
+                }
+
+                else
+                {
+                    printf("Password tidak valid karena kepanjangan. Masukkan lagi yuk!");
+                    PassCheck = false;
+                }
+            }
+        }
+        User NewUser = {name, pass};
+        printf("\nPengguna telah berhasil terdaftar. Masuk untuk menikmati fitur-fitur BurBir.\n");
+        insertLastUser(users, NewUser);
+    }
+
+    else
+    {
+        printf("\nAnda sudah masuk, keluar terlebih dahulu untuk mendaftar\n");
+    }
+}
+
+void MASUK(UserList *users)
+{
+    if (!isLogin)
+    {
+        printf("\n\nMasukkan Nama:\n");
+        STARTCOMMAND();
+
+        Word nama = currentWord;
+        int IndexUser = indexOfUser(*users, nama);
+
+        if (IndexUser == -1)
+        {
+            printf("Wah, nama yang Anda cari tidak ada. Masukkan nama lain!");
+            MASUK(users);
+        }
+        else
+        {
+            User terdaftar = Pengguna(*users, IndexUser);
+            Word CorrectPass = terdaftar.password;
+            boolean PassCheck = false;
+
+            while (!PassCheck)
+            {
+                printf("\n\nMasukkan kata sandi:\n");
+                STARTCOMMAND();
+
+                Word Inputpass = currentWord;
+                if (compareWord(CorrectPass, Inputpass))
+                {
+                    PassCheck = true;
+                }
+                else
+                {
+                    PassCheck = false;
+                    printf("Wah, kata sandi yang Anda masukkan belum tepat. Periksa kembali kata sandi Anda!");
+                }
+            }
+            printf("Anda telah berhasil masuk dengan nama pengguna ");
+            PrintWord(terdaftar.username);
+            printf(". Mari menjelajahi BurBir bersama Ande-Ande Lumut!");
+            currentUser = terdaftar;
+        }
+    }
+
+    else
+    {
+        printf("\nAnda sudah masuk, keluar terlebih dahulu untuk mendaftar\n");
+    }
+}
+
+void KELUAR()
+{
+    if (!isLogin)
+    {
+        printf("\nAnda belum login! Masuk terlebih dahulu untuk menikmati layanan BurBir.\n");
+    }
+    else
+    {
+        isLogin = false;
+        printf("\nAnda berhasil logout. Sampai jumpa di pertemuan berikutnya!\n");
+    }
+}
+
 
 #endif
